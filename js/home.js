755c2e4caa3b84a557cfbd5825598d6f3cda3448
AgayevAmin <aghayevamin@gmail.com>
2024-01-18 16:42:48 +0400

    js7-12

---
+++ js/home.js
@@ -0,0 +1,67 @@
+//////////////////////////7///////////////////////////////
+
+// function changeStyle() {
+//     var text = document.getElementById("Text");
+//     text.style.fontSize = "35px";
+//     text.style.color = "#7F7FFF";
+//   }
+  // ////////////////////8/////////////////////////
+  
+//   function changeBackgroundColor() {
+//     var p3 = document.getElementById("p3");
+  
+//     p3.style.backgroundColor = "lightblue";
+//   }
+  
+//   function changeBackgroundColor() {
+//     var p3 = document.getElementById("p3");
+//     para3.style.backgroundColor = "lightblue";
+//   }
+ 
+  
+  ///////////////////////9////////////////////////////////
+  
+  // function over() {
+  //   var text = document.getElementById("Text");
+  
+  //   var boldWords = text.getElementsByTagName("strong");
+  
+  //   var secilmis = "highlight";
+  
+  //   for (var i = 0; i < boldWords.length; i++) {
+  //     boldWords[i].classList.add(secilmis);
+  //   }
+  // }
+// 
+  /////////////////////////////10///////////////////////////////
+//   function highlightBoldWords() {
+//     var text = document.getElementById("Text2");
+//     var boldWords = text.getElementsByTagName("strong");
+//     var highlightClass = "highlight";
+  
+//     for (var i = 0; i < boldWords.length; i++) {
+//       boldWords[i].classList.add(highlightClass);
+//     }
+//   }
+/////////////////////////////11///////////////////////////
+
+// function strinqiYoxla(strinq) {
+//   return strinq.trim() === "";
+// }
+
+// var emptyStrinq = "";
+// var follStrinq = "Bu strinq doludur.";
+
+// console.log(strinqiYoxla(emptyStrinq)); 
+// console.log(strinqiYoxla(follStrinq)); 
+
+////////////////////////////// 12 /////////////////////////////////////
+
+// function capitalized(str) {
+//   return str.charAt(0).toUpperCase() + str.slice(1);
+// }
+
+// var inputString = "coders";
+// var capitalizedString = capitalized(inputString);
+
+// console.log(capitalizedString); 
